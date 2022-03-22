b271301df4387d8eb4f676f6cc90757a88584c8f
Rufus14 <titoandrade00@gmail.com>
2022-03-22 23:05:24 +0000

    Sum feita

--- converter.py
+++ converter.py
@@ -23,31 +23,34 @@ def isList2 (dados):
 def extraList2 (dados): #campo extra do tipo de lista 2 {2,4} -> 4}
         return re.search(r'\d}', dados)
 
-def calculaAvg(dados, campo, separator_lista):
-    lista = re.split(separator_lista, dados)
+def calculaAvg(dados, campo, separator):
+    lista = re.split(separator, dados)
     for i in range(len(lista)):
         if isInt(lista[i]):
             lista[i] = int(lista[i])
         else:
             lista[i] = float(lista[i])
     media = sum(lista)/len(lista)
-    campo = re.sub(r'{\d,\d}::avg', r'_', campo)
+    campo = re.sub(r'::avg', r'_', campo)
     return ("\"" + campo + "\": " + str(media))
 
 
-def calculaSum(dados, campo, separator_lista):
-    lista = re.split(separator_lista, dados)
-    for i in range(len(lista)):
-       if isInt(lista[i]):
-            lista[i] = int(lista[i])
+def calculaSum(dados, campo):
+    lista = list()
+    for dado in dados:
+       if isInt(dado):
+            lista.append (int(dado))
        else:
-            lista[i] = float(lista[i])
+            pass
+    
     soma = sum(lista)
-    campo = re.sub(r'{\d,\d}::sum', r'_', campo)
+    #campo = re.sub(r'::sum', r'_', campo)
+    campo = re.sub(r'{\d+(}::sum)?', r'_sum', campo)
+    #print (campo) 
     return ("\"" + campo + "\": " + str(soma))
 
 
-def converter (csv, fileOutput, separator, separator_lista):
+def converter (csv, fileOutput, separator):
         file = open(csv)
         fileOutput = open(fileOutput, 'w')
         first_line = file.readline()
@@ -62,14 +65,51 @@ def converter (csv, fileOutput, separator, separator_lista):
                 output += ("{\n")
                 valores = re.split(separator, line.strip())
                 for i in range(len(campos)):
-                        #print(campos[i])
-                        if not (isList1(campos[i])):
-                                if campos[i] == '':
-                                        pass
-                                elif extraList2(campos[i]):
-                                        pass
+
+                        if isList1(campos[i]):
                                 
-                                elif isList2(campos[i]):
+                                if re.search(r'[Aa][Vv][Gg]', campos[i]):
+                                        #print ("aqui1")
+                                        valor = valores[i]
+                                        output += calculaAvg(valor, campos[i], separator)
+                                        output += ",\n"
+                                if re.search(r'[Ss][Uu][Mm]', campos[i]):
+                                        #print ("aqui2")
+                                        valor = valores[i:]
+                                        output += calculaSum(valor, campos[i])
+                                        output += ",\n"
+
+                                else:
+                                        result = re.search(r'(\d+)', campos[i])
+                                        #print (result.group(1))
+                                        x=0
+                                        output += ("\"" + campos[i] + "\":" + "[")
+                                        while (x+1<int(result.group(1))): 
+                                                output +=( valores[i+x] + ",")
+                                                x+=1
+                                        output += (valores[i+x]+"]\n")
+                        
+                        elif campos[i] == '':
+                                pass
+                        elif extraList2(campos[i]):
+                                pass
+                                
+                        elif isList2(campos[i]):
+
+                                if re.search(r'[Aa][Vv][Gg]', campos[i+1]):
+                                        #print ("aqui1")
+                                        valor = valores[i]
+                                        output += calculaAvg(valor, campos[i], separator)
+                                        output += ",\n"
+                                if re.search(r'[Ss][Uu][Mm]', campos[i+1]):
+                                        #print ("aqui2")
+                                        valor = valores[i:]
+                                        #print (valores[i:] , "***")
+                                        output += calculaSum(valor, campos[i])
+                                        output += ",\n"
+
+                                else:
+                                        #print ("que estas aqui a fazer crlh?")
                                         #r1 = int ((re.search(r'(\d+)', campos[i])).group(1)) #{2
                                         r2 = int ((re.search(r'(\d+)', campos[i+1])).group(1)) #4}
 
@@ -82,55 +122,10 @@ def converter (csv, fileOutput, separator, separator_lista):
                                         output = re.sub(r',+\]',']', output)
                                         
                                         
-                                        
-                                        #y=r1#-1
-                                        #while (y < r2-1):
-                                                #print ("****"+valores[i+y])
-                                                #if (valores[i+y] == ''):
-                                                       # pass                                                                
-                                                #else:
-                                                        #output += ( "," + valores[i+y] ) 
-                                                        #print("while2:" + valores[i+y])  
-                                                       # y+=1
-                                        #output += ("," + valores[i+y]+"]\n")
-                                        #if (valores[i+y] == ''):
-                                                #output += (valores[i+y]+"]\n")
-                                        #else:
-                                                #output += ("," + valores[i+y]+"]\n")
-                                        #print("fim:" + valores[i+y])
-                                
-                                else:
-                                        output += ("\"" + campos[i] + "\": \"" + valores[i] + "\",\n")
-                        elif isList1(campos[i]):
-                                result = re.search(r'(\d+)', campos[i])
-                                #print (result.group(1))
-                                x=0
-                                output += ("\"" + campos[i] + "\":" + "[")
-                                while (x+1<int(result.group(1))): 
-                                        output +=( valores[i+x] + ",")
-                                        x+=1
-                                output += (valores[i+x]+"]\n")
-                                                
-                                
 
+                        else:
+                                output += ("\"" + campos[i] + "\": \"" + valores[i] + "\",\n")           
 
-
-
-#                        else:
-#                                valor = re.sub(r"\(", r"", valores[i])  #re.sub(pattern, repl, string, count=0, flags=0)
-#                                valor = re.sub(r"\)", r"", valor)       #Return the string obtained by replacing the leftmost non-overlapping occurrences of pattern in string by the replacement repl. If the pattern isn’t found, string is returned unchanged.                                    
-#
-#                                #else:
-#                                lista = re.split(separator_lista, valor)
-#                                campo = re.sub(r'\*', r"", campos[i])
-#                                output += ("\"" + campo + "\": ")
-#                                output += "["
-#                                for index in range(len(lista)):
-#                                        output += str(lista[index])
-#                                        output += ","
-#                                output += "]"
-#                                output = re.sub(r',]', r']', output)
-#                                output += (",\n")
                 output += ("},\n")
         
         output += ("]")
@@ -140,19 +135,7 @@ def converter (csv, fileOutput, separator, separator_lista):
         fileOutput.write(output)    
 
 csv = input('Insira o ficheiro CSV que pretende ler: ')
-json = input('Insira o nome do ficheiro JSON: ')
+json = input('Insira o nome do ficheiro JSON que pretende criar: ')
 
-converter(csv, json, ",", ";")
+converter(csv, json, ",")
 print('Conversão realizada com sucesso!')
-
-
-""" separator = input('CSV separado por:\n1) ;\n2) ,\n')
-if separator == "1":
-    converter(csv, json, ";", ",")
-    print('Conversão realizada com sucesso!')
-elif separator == "2":
-    converter(csv, json, ",", ";")
-    print('Conversão realizada com sucesso!')
-else :
-    print("Opção inválida")            
- """                                
