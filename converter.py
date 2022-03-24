8a943aa4e36f9ca6428f08750677162bd33936de
Rufus14 <titoandrade00@gmail.com>
2022-03-24 12:07:44 +0000

    Terminado o que é pedido

--- converter.py
+++ converter.py
@@ -10,9 +10,6 @@ from sqlalchemy import outparam
 
 def isInt (dados):
         return re.search(r'^(-)?[0-9]+$', dados)
-        
-def isNumber (dados):
-        return re.search(r'^(-)?[0-9]+(\.[0-9]+)?$', dados)
 
 def isList1 (dados): 
         return re.search(r'\w+{\d}', dados)  #[A-Za-z]
@@ -23,15 +20,15 @@ def isList2 (dados):
 def extraList2 (dados): #campo extra do tipo de lista 2 {2,4} -> 4}
         return re.search(r'\d}', dados)
 
-def calculaAvg(dados, campo, separator):
-    lista = re.split(separator, dados)
-    for i in range(len(lista)):
-        if isInt(lista[i]):
-            lista[i] = int(lista[i])
-        else:
-            lista[i] = float(lista[i])
+def calculaAvg(dados, campo):
+    lista = list()
+    for dado in dados:
+       if isInt(dado):
+            lista.append (int(dado))
+       else:
+            pass
     media = sum(lista)/len(lista)
-    campo = re.sub(r'::avg', r'_', campo)
+    campo = re.sub(r'{\d+(}::avg)?', r'_avg', campo)
     return ("\"" + campo + "\": " + str(media))
 
 
@@ -42,12 +39,10 @@ def calculaSum(dados, campo):
             lista.append (int(dado))
        else:
             pass
-    
     soma = sum(lista)
-    #campo = re.sub(r'::sum', r'_', campo)
     campo = re.sub(r'{\d+(}::sum)?', r'_sum', campo)
-    #print (campo) 
     return ("\"" + campo + "\": " + str(soma))
+
 
 
 def converter (csv, fileOutput, separator):
@@ -69,24 +64,22 @@ def converter (csv, fileOutput, separator):
                         if isList1(campos[i]):
                                 
                                 if re.search(r'[Aa][Vv][Gg]', campos[i]):
-                                        #print ("aqui1")
-                                        valor = valores[i]
-                                        output += calculaAvg(valor, campos[i], separator)
+                                        valor = valores[i:]
+                                        output += calculaAvg(valor, campos[i])
                                         output += ",\n"
-                                if re.search(r'[Ss][Uu][Mm]', campos[i]):
-                                        #print ("aqui2")
+                                elif re.search(r'[Ss][Uu][Mm]', campos[i]):
                                         valor = valores[i:]
                                         output += calculaSum(valor, campos[i])
                                         output += ",\n"
 
                                 else:
                                         result = re.search(r'(\d+)', campos[i])
-                                        #print (result.group(1))
                                         x=0
                                         output += ("\"" + campos[i] + "\":" + "[")
                                         while (x+1<int(result.group(1))): 
                                                 output +=( valores[i+x] + ",")
                                                 x+=1
+                                        print ("como???")
                                         output += (valores[i+x]+"]\n")
                         
                         elif campos[i] == '':
@@ -97,20 +90,15 @@ def converter (csv, fileOutput, separator):
                         elif isList2(campos[i]):
 
                                 if re.search(r'[Aa][Vv][Gg]', campos[i+1]):
-                                        #print ("aqui1")
-                                        valor = valores[i]
-                                        output += calculaAvg(valor, campos[i], separator)
-                                        output += ",\n"
-                                if re.search(r'[Ss][Uu][Mm]', campos[i+1]):
-                                        #print ("aqui2")
                                         valor = valores[i:]
-                                        #print (valores[i:] , "***")
+                                        output += calculaAvg(valor, campos[i])
+                                        output += ",\n"
+                                elif re.search(r'[Ss][Uu][Mm]', campos[i+1]):
+                                        valor = valores[i:]
                                         output += calculaSum(valor, campos[i])
                                         output += ",\n"
 
                                 else:
-                                        #print ("que estas aqui a fazer crlh?")
-                                        #r1 = int ((re.search(r'(\d+)', campos[i])).group(1)) #{2
                                         r2 = int ((re.search(r'(\d+)', campos[i+1])).group(1)) #4}
 
                                         x=0 
